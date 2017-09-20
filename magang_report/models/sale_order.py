3a71fa02478e614d7eeff27178afb5a9a7f0902a
hilmy74 <hilmymuhammad74@gmail.com>
2017-09-20 16:14:45 +0700

    perbaikan modul magang_report

--- magang_report/models/sale_order.py
+++ magang_report/models/sale_order.py
@@ -1,7 +1,12 @@
-# from odoo import api, fields, models, _
-# 
-# class ProductTemplate(models.Model):
-#     _inherit= 'product.product'
-#     
-#     tin_dus=fields.Float(related='product_tmpl_id.tin_dus')
+from odoo import api, fields, models, _
+ 
+class ProductTemplate(models.Model):
+    _inherit= 'product.product'
+     
+    tin_dus=fields.Float(related='product_tmpl_id.tin_dus')
+    
+class ProductTemplates(models.Model):
+    _inherit= 'product.template'
+    
+    tin_dus = fields.Float('Tin/Dus')
     
